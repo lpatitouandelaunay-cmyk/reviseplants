9ce500408b45df381ce5b8167e7952b55b4d8fc3
lpatitouandelaunay-cmyk <lpa.titouan.delaunay@gmail.com>
2025-11-22 23:42:09 +0100

    Create save.php

---
+++ save.php
@@ -0,0 +1,20 @@
+<?php
+// Fix sessions OVH
+ini_set('session.save_path', __DIR__ . '/sessions');
+if (!is_dir(__DIR__ . '/sessions')) mkdir(__DIR__ . '/sessions');
+session_start();
+
+if (!($_SESSION['logged'] ?? false)) exit;
+
+$path = __DIR__ . '/data/gifts.json';
+$gifts = json_decode(file_get_contents($path), true);
+
+$gifts[] = [
+    'name' => $_POST['name'],
+    'description' => $_POST['description'],
+    'url' => $_POST['url']
+];
+
+file_put_contents($path, json_encode($gifts, JSON_PRETTY_PRINT));
+
+echo "OK";
